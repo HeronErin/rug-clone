23703831788f3e00dfc5ece83955050e12b4f938
Trevor Spiteri <trevor.spiteri@um.edu.mt>
2018-02-20 01:15:45 +0100

    print start and finish time in coverage.sh

--- coverage.sh
+++ coverage.sh
@@ -43,16 +43,18 @@ sed -i$SUFFIX -n -e "$EXTRACT_SCRIPT" src/**/*.rs
 
 # generate coverage.report
 FILTER_SCRIPT='
-1i\
--*- mode: compilation; default-directory: "'"$PWD"'"-*-\
-
 s/ - /: /               # make lines friendly with emacs compilation mode
 /hits: 0/p              # print zero coverage-lines immediately
 H                       # hold everything
 ${x;p}                  # at the end of the file, print the hold
 '
-cargo tarpaulin -v --features serde --ignore-tests |&
-    sed -n -e "$FILTER_SCRIPT" > coverage.report
+(
+    printf '%s*- mode: compilation; default-directory: "%s" -*-\n' - "$PWD"
+    printf 'Compilation started at %s\n\n' "$(date)"
+    cargo tarpaulin -v --features serde --ignore-tests |&
+        sed -n -e "$FILTER_SCRIPT"
+    printf '\nCompilation finished at %s\n' "$(date)\n"
+) > coverage.report
 
 # restore original sources
 for f in src/**/*.rs$SUFFIX; do
