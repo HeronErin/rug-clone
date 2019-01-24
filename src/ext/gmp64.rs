198e370ef12da958e61d40ef98a7057570fc1877
Trevor Spiteri <trevor.spiteri@um.edu.mt>
2019-01-24 06:44:48 +0100

    fix clippy warnings on windows x86_64

--- src/ext/gmp64.rs
+++ src/ext/gmp64.rs
@@ -106,11 +106,13 @@ pub unsafe fn mpz_init_set_i64(rop: *mut mpz_t, i: i64) {
     }
 }
 
+#[cfg_attr(feature = "cargo-clippy", allow(clippy::identity_conversion))]
 #[inline]
 pub unsafe fn mpz_init_set_u32(rop: *mut mpz_t, u: u32) {
     gmp::mpz_init_set_ui(rop, u.into());
 }
 
+#[cfg_attr(feature = "cargo-clippy", allow(clippy::identity_conversion))]
 #[inline]
 pub unsafe fn mpz_init_set_i32(rop: *mut mpz_t, i: i32) {
     gmp::mpz_init_set_si(rop, i.into());
