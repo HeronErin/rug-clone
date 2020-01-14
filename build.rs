e1589d8683ed477053bb5dd6c858998d26c2a87d
Trevor Spiteri <trevor.spiteri@um.edu.mt>
2020-01-14 16:40:55 +0100

    remove unused code in build.rs

--- build.rs
+++ build.rs
@@ -5,29 +5,11 @@
 // the copyright notice and this notice are preserved. This file is
 // offered as-is, without any warranty.
 
-#![allow(dead_code)]
-#![allow(unused_variables)]
 #![cfg_attr(feature = "fail-on-warnings", deny(warnings))]
 
-use std::{
-    env,
-    ffi::OsString,
-    fs::{self, File},
-    io::{Result as IoResult, Write},
-    path::{Path, PathBuf},
-    process::{Command, Stdio},
-};
-
-struct Environment {
-    out_dir: PathBuf,
-    rustc: OsString,
-}
+use std::env;
 
 fn main() {
-    let env = Environment {
-        out_dir: PathBuf::from(cargo_env("OUT_DIR")),
-        rustc: cargo_env("RUSTC"),
-    };
     if env::var_os("CARGO_FEATURE_GMP_MPFR_SYS").is_some() {
         let bits =
             env::var_os("DEP_GMP_LIMB_BITS").expect("DEP_GMP_LIMB_BITS not set by gmp-mfpr-sys");
@@ -39,85 +21,4 @@ fn main() {
         }
         println!("cargo:rustc-cfg=gmp_limb_bits_{}", bits);
     }
-}
-
-impl Environment {
-    fn check_feature(&self, name: &str, contents: &str, nightly_features: Option<&str>) {
-        let try_dir = self.out_dir.join(format!("try_{}", name));
-        let filename = format!("try_{}.rs", name);
-        create_dir_or_panic(&try_dir);
-        println!("$ cd {:?}", try_dir);
-
-        enum Iteration {
-            Stable,
-            Unstable,
-        }
-        for i in &[Iteration::Stable, Iteration::Unstable] {
-            let s;
-            let file_contents = match *i {
-                Iteration::Stable => contents,
-                Iteration::Unstable => match nightly_features {
-                    Some(features) => {
-                        s = format!("#![feature({})]\n{}", features, contents);
-                        &s
-                    }
-                    None => continue,
-                },
-            };
-            create_file_or_panic(&try_dir.join(&filename), file_contents);
-            let mut cmd = Command::new(&self.rustc);
-            cmd.current_dir(&try_dir)
-                .stdout(Stdio::null())
-                .stderr(Stdio::null())
-                .args(&[&filename, "--emit=dep-info,metadata"]);
-            println!("$ {:?} >& /dev/null", cmd);
-            let status = cmd
-                .status()
-                .unwrap_or_else(|_| panic!("Unable to execute: {:?}", cmd));
-            if status.success() {
-                println!("cargo:rustc-cfg={}", name);
-                if let Iteration::Unstable = *i {
-                    println!("cargo:rustc-cfg=nightly_{}", name);
-                }
-                break;
-            }
-        }
-
-        remove_dir_or_panic(&try_dir);
-    }
-}
-
-fn cargo_env(name: &str) -> OsString {
-    env::var_os(name)
-        .unwrap_or_else(|| panic!("environment variable not found: {}, please use cargo", name))
-}
-
-fn remove_dir(dir: &Path) -> IoResult<()> {
-    if !dir.exists() {
-        return Ok(());
-    }
-    assert!(dir.is_dir(), "Not a directory: {:?}", dir);
-    println!("$ rm -r {:?}", dir);
-    fs::remove_dir_all(dir)
-}
-
-fn remove_dir_or_panic(dir: &Path) {
-    remove_dir(dir).unwrap_or_else(|_| panic!("Unable to remove directory: {:?}", dir));
-}
-
-fn create_dir(dir: &Path) -> IoResult<()> {
-    println!("$ mkdir -p {:?}", dir);
-    fs::create_dir_all(dir)
-}
-
-fn create_dir_or_panic(dir: &Path) {
-    create_dir(dir).unwrap_or_else(|_| panic!("Unable to create directory: {:?}", dir));
-}
-
-fn create_file_or_panic(filename: &Path, contents: &str) {
-    println!("$ printf %s {:?}... > {:?}", &contents[0..20], filename);
-    let mut file =
-        File::create(filename).unwrap_or_else(|_| panic!("Unable to create file: {:?}", filename));
-    file.write_all(contents.as_bytes())
-        .unwrap_or_else(|_| panic!("Unable to write to file: {:?}", filename));
 }
