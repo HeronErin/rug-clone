d422762828606ce28da9e2caa20aa649e89557c9
Trevor Spiteri <tspiteri@ieee.org>
2022-11-29 20:51:24 +0100

    implement more traits for BorrowFloat

--- src/float/borrow.rs
+++ src/float/borrow.rs
@@ -14,9 +14,15 @@
 // a copy of the GNU General Public License along with this program. If not, see
 // <https://www.gnu.org/licenses/>.
 
+use crate::float::OrdFloat;
 use crate::Float;
+use core::borrow::Borrow;
+use core::convert::AsRef;
+use core::fmt::{
+    Binary, Debug, Display, Formatter, LowerExp, LowerHex, Octal, Pointer, Result as FmtResult,
+    UpperExp, UpperHex,
+};
 use core::marker::PhantomData;
-use core::mem::ManuallyDrop;
 use core::ops::Deref;
 use gmp_mpfr_sys::mpfr::mpfr_t;
 
@@ -37,14 +43,14 @@ use gmp_mpfr_sys::mpfr::mpfr_t;
 /// assert_eq!(f, 4.2);
 /// assert_eq!(*neg, -4.2);
 /// ```
-#[derive(Debug)]
+#[derive(Clone, Copy)]
 #[repr(transparent)]
 pub struct BorrowFloat<'a> {
-    inner: ManuallyDrop<Float>,
+    inner: mpfr_t,
     phantom: PhantomData<&'a Float>,
 }
 
-impl BorrowFloat<'_> {
+impl<'a> BorrowFloat<'a> {
     /// Create a borrow from a raw [MPFR floating-point number][mpfr_t].
     ///
     /// # Safety
@@ -69,9 +75,9 @@ impl BorrowFloat<'_> {
     /// let borrow = unsafe { BorrowFloat::from_raw(raw) };
     /// assert_eq!(f, *borrow);
     /// ```
-    pub unsafe fn from_raw<'a>(raw: mpfr_t) -> BorrowFloat<'a> {
+    pub unsafe fn from_raw(raw: mpfr_t) -> BorrowFloat<'a> {
         BorrowFloat {
-            inner: ManuallyDrop::new(unsafe { Float::from_raw(raw) }),
+            inner: raw,
             phantom: PhantomData,
         }
     }
@@ -81,6 +87,87 @@ impl Deref for BorrowFloat<'_> {
     type Target = Float;
     #[inline]
     fn deref(&self) -> &Float {
-        &self.inner
+        let ptr = cast_ptr!(&self.inner, Float);
+        // Safety: the inner pointer is valid for the duration of the lifetime.
+        unsafe { &*ptr }
     }
 }
+
+impl Borrow<Float> for BorrowFloat<'_> {
+    #[inline]
+    fn borrow(&self) -> &Float {
+        &**self
+    }
+}
+
+impl AsRef<Float> for BorrowFloat<'_> {
+    #[inline]
+    fn as_ref(&self) -> &Float {
+        &**self
+    }
+}
+
+impl AsRef<OrdFloat> for BorrowFloat<'_> {
+    #[inline]
+    fn as_ref(&self) -> &OrdFloat {
+        self.as_ord()
+    }
+}
+
+impl Pointer for BorrowFloat<'_> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        Pointer::fmt(&&**self, f)
+    }
+}
+
+impl Display for BorrowFloat<'_> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        Display::fmt(&**self, f)
+    }
+}
+
+impl Debug for BorrowFloat<'_> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        Debug::fmt(&**self, f)
+    }
+}
+
+impl Binary for BorrowFloat<'_> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        Binary::fmt(&**self, f)
+    }
+}
+
+impl Octal for BorrowFloat<'_> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        Octal::fmt(&**self, f)
+    }
+}
+
+impl LowerHex for BorrowFloat<'_> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        LowerHex::fmt(&**self, f)
+    }
+}
+
+impl UpperHex for BorrowFloat<'_> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        UpperHex::fmt(&**self, f)
+    }
+}
+
+impl LowerExp for BorrowFloat<'_> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        LowerExp::fmt(&**self, f)
+    }
+}
+
+impl UpperExp for BorrowFloat<'_> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        UpperExp::fmt(&**self, f)
+    }
+}
+
+// Safety: mpfr_t is thread safe as guaranteed by the MPFR library.
+unsafe impl Send for BorrowFloat<'_> {}
+unsafe impl Sync for BorrowFloat<'_> {}
